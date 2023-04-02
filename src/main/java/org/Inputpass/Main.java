ad329f9d41151ed5d6e1674b0d3029949a3cdd1a
Your Name <you@example.com>
2023-04-02 18:38:27 +0200

    13v7906

--- src/main/java/org/Inputpass/Main.java
+++ src/main/java/org/Inputpass/Main.java
@@ -1,40 +1,34 @@
 package org.Inputpass;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.Console;
 import java.util.concurrent.*;
 
 public class Main {
 
 	public static final String PASSWORD = "123";
 	public static final int MAX_ATTEMPTS = 3;
-	public static final int TIMEOUT = 15;
-	public static final int SLEEP = 50;
+	public static final int TIMEOUT = 5;
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
+		Console console = System.console();
+		if (console == null) {
+			System.err.println("No console available. Terminating.");
+			System.exit(1);
+		}
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 
 		for (int attempts = 1; attempts <= MAX_ATTEMPTS; attempts++) {
-
 			System.out.printf("Enter password (%d/%d):%n", attempts, MAX_ATTEMPTS);
-
 			Future<String> future = executor.submit(() -> {
-				try {
-					while (System.in.available() == 0) {
-						Thread.sleep(SLEEP);
-					}
-					return scanner.nextLine();
-				} catch (IOException | InterruptedException e) {
-					return null;
-				}
+				char[] passwordChars = console.readPassword();
+				return new String(passwordChars);
 			});
 
 			try {
 				String inputPassword = future.get(TIMEOUT, TimeUnit.SECONDS);
 
 				if (PASSWORD.equals(inputPassword)) {
+					System.out.println("1. STOP");
 					System.out.println("Access granted.");
 					executor.shutdown();
 					return;
@@ -44,14 +38,15 @@ public class Main {
 			} catch (TimeoutException e) {
 				System.out.println("Timeout. Please try again.");
 				future.cancel(true);
+				System.out.println("\n");
 				break;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-
+		System.out.println("2. STOP");
 		System.out.println("Program terminated.");
+
 		executor.shutdown();
 	}
-
 }
